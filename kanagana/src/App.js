ec1bb1fdec6cb83e407d8aa12ca5fb269725dca4
Chinh Nguyen <nguyenchuu@yahoo.com>
2025-01-07 17:59:43 -0600

    hiragana and katakana text on screen

--- kanagana/src/App.js
+++ kanagana/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { MantineProvider, Button, Container, Group, Title } from '@mantine/core';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider>
+      <Container
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          textAlign: 'center',
+          background: 'linear-gradient(to bottom,rgb(154, 208, 221),rgb(8, 132, 214))',
+        }}
+      >
+        <Title order={1}>ひらがな</Title>
+        <Title order={1}>と</Title>
+        <Title order={1}>カタカナ</Title>
+      </Container>
+    </MantineProvider>
   );
 }
 
