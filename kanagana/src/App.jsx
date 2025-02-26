37700db2dc6c7e05fd3a8db84c1623986c1caaf2
Chinh Nguyen <nguyenchuu@yahoo.com>
2025-02-25 21:50:13 -0600

    Added clickability to select hiragana/katakana

--- kanagana/src/App.jsx
+++ kanagana/src/App.jsx
@@ -2,11 +2,22 @@ import { useState } from 'react'
 
 function App() {
 
+  const [showText, setShowText] = useState(true);
+
+  const handleClick = (input) => {
+    setShowText(false);
+    console.log(`${input} was clicked`);
+  };
+
   return (
     <>
       <div className="main-container">
-        <h1>ひらがな</h1>
-        <h1>カタカナ</h1>
+        {showText && (
+          <>
+            <h1 onClick={() => handleClick("hiragana")}>ひらがな</h1>
+            <h1 onClick={() => handleClick("katakana")}>カタカナ</h1>
+          </>
+        )}
       </div>
     </>
   )
