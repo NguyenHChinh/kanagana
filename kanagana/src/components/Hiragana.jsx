b8b054683df8f0a87fe2488aa9bfc1d7aa368a4a
Chinh Nguyen <36144798+NguyenHChinh@users.noreply.github.com>
2025-04-20 22:30:21 -0500

    Added random Hiragana function; display said Hiragana

--- kanagana/src/components/Hiragana.jsx
+++ kanagana/src/components/Hiragana.jsx
@@ -1,10 +1,16 @@
+import { useState, useEffect, useCallback } from "react";
+import hiraganaCharacters from '../data/hiraganaCharacters.json';
+
+const getRandomHiragana = () => {
+    return Object.keys(hiraganaCharacters)[Math.floor(Math.random() * Object.keys(hiraganaCharacters).length)];
+  };
 
 function Hiragana() {
-    
+    const [currentHiragana, setCurrentHiragana] = useState(getRandomHiragana());
+
     return(
         <>
-            <h1>You are currently in the Hiragana section!</h1>
-            <h1>However, this is still W.I.P.!</h1>
+            <h1>{currentHiragana}</h1>
         </>
     )
 }
