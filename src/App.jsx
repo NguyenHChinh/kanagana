4f772c9ac0f8a483b73b635d722922b460aa072e
Chinh Nguyen <nguyenchuu@yahoo.com>
2025-04-23 20:39:54 -0500

    fix: replace BrowserRouter with HashRouter for routing consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import Practice from "./pages/Practice";
 import Home from './pages/Home.jsx';
@@ -19,17 +19,17 @@ function App() {
   };
 
   return (
-    <BrowserRouter basename="/kanagana">
-      <Navbar/>
+    <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/practice" element={<PracticeSelect/>}/>
-        <Route path="/practice/:kanaType" element={<Practice/>}/>
-        <Route path="/progress" element={<Progress/>}/>
-        <Route path="/about" element={<About/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/practice" element={<PracticeSelect />} />
+        <Route path="/practice/:kanaType" element={<Practice />} />
+        <Route path="/progress" element={<Progress />} />
+        <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </Router>
+  );
 }
 
 export default App
