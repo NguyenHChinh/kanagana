37c968637bebabfa79aff4f6adb08d68d28074d9
Chinh Nguyen <nguyenchuu@yahoo.com>
2025-04-28 20:01:57 -0500

    feat: enhance Hiragana component with user input evaluation and feedback animations

--- src/components/Hiragana.jsx
+++ src/components/Hiragana.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import hiraganaCharacters from '../data/hiraganaCharacters.json';
 import KanaBox from './KanaBox';
 import Keyboard from './Keyboard';
@@ -12,18 +12,52 @@ function Hiragana() {
     const [currentHiragana, setCurrentHiragana] = useState(getRandomHiragana());
     const [userInput, setUserInput] = useState("");
     const [isCorrect, setIsCorrect] = useState(false);
+    const [evaluation, setEvaluation] = useState("");
     const [trigger, setTrigger] = useState(0);
+
+    const shakeTimeoutRef = useRef(null);
     
     function handleSubmit() {
-        const correctAnswers = hiraganaCharacters[currentHiragana];
-        const isCorrect = correctAnswers.includes(userInput);
-    
         if (isCorrect) {
             setCurrentHiragana(getRandomHiragana());
             setUserInput("");
             setTrigger(prev => prev + 1);
+            setIsCorrect(false);
+
+            if (shakeTimeoutRef.current) {
+                clearTimeout(shakeTimeoutRef.current);
+                shakeTimeoutRef.current = null;
+            }
+            setEvaluation("");
+            return;
         }
-        setIsCorrect(isCorrect);
+
+        const correctAnswers = hiraganaCharacters[currentHiragana];
+        const userAnswer = userInput.trim().toLowerCase();
+
+        const temp = correctAnswers.includes(userAnswer);
+
+        if (temp) {
+            console.log("Correct answer!");
+            setIsCorrect(true);
+        }
+        else {
+            console.log("Wrong answer!");
+            setEvaluation("");
+
+            setTimeout(() => {
+                setEvaluation("shake");
+    
+                if (shakeTimeoutRef.current) {
+                    clearTimeout(shakeTimeoutRef.current);
+                }
+    
+                shakeTimeoutRef.current = setTimeout(() => {
+                    setEvaluation("");
+                    shakeTimeoutRef.current = null;
+                }, 3000);
+            }, 0);
+        }
     }
 
     return(
@@ -34,6 +68,8 @@ function Hiragana() {
                     <div className="word-container">
                         <KanaBox
                             char={currentHiragana}
+                            key={`${trigger}`}
+                            status={isCorrect ? undefined : evaluation}
                         />
                     </div>
                 </div>
@@ -43,6 +79,7 @@ function Hiragana() {
                 sendData={setUserInput}
                 onEnter={handleSubmit}
                 resetSignal={trigger}
+                isCorrect={isCorrect}
             />
         </>
     )
