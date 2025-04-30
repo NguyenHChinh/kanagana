4bf1926b5e9f7513cc4a3a1446e58b2c529beac3
Chinh Nguyen <nguyenchuu@yahoo.com>
2025-04-29 19:56:59 -0500

    feat: update KanaSelect and Practice components for improved structure and consistency

--- src/components/KanaSelect.jsx
+++ src/components/KanaSelect.jsx
@@ -7,13 +7,25 @@ function KanaSelect() {
     return(
         <div className="practice-select-page">
             <h1 className="title">What do you want to practice?</h1>
-            <p className="subtitle">Pick a kana set to begin practicing.</p>
+            <h2 className="subtitle">Pick a kana set to begin practicing.</h2>
+            <p className="description">Single Characters</p>
             <div className="kana-select-container">
                 <div className="kana-card" onClick={() => navigate('/practice/hiragana')}>
-                    <h2>ひらがな</h2>
+                    <h2>か</h2>
                     <p>Hiragana</p>
                 </div>
                 <div className="kana-card" onClick={() => navigate('/practice/katakana')}>
+                    <h2>カ</h2>
+                    <p>Katakana</p>
+                </div>
+            </div>
+            <p className="description">Full Words</p>
+            <div className="kana-select-container">
+                <div className="kana-card" onClick={() => navigate('/practice/hiragana-words')}>
+                    <h2>ひらがな</h2>
+                    <p>Hiragana</p>
+                </div>
+                <div className="kana-card" onClick={() => navigate('/practice/katakana-words')}>
                     <h2>カタカナ</h2>
                     <p>Katakana</p>
                 </div>
--- src/pages/Practice.jsx
+++ src/pages/Practice.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import KanaKeyboard from "../components/KanaKeyboard";
 import WordSelector from "../components/WordSelector";
 import Hiragana from '../components/Hiragana.jsx';
-import Katakana from '../components/Katakana.jsx';
+import KatakanaWords from '../components/KatakanaWords.jsx';
 
 function Practice() {
     const { kanaType } = useParams();
@@ -12,7 +12,7 @@ function Practice() {
     return(
         <>
             {kanaType === "hiragana" && <Hiragana />}
-            {kanaType === "katakana" && <Katakana />}
+            {kanaType === "katakana-words" && <KatakanaWords />}
         </>
     )
 }
